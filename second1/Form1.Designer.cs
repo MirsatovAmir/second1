e058082e58d71e6347de4a3e142bcf121d0ea671
MirsatovAmir <amirmirsatov@gmail.com>
2022-09-25 22:28:12 +0500

    зеленая форма и нет кнопки закрыть

--- second1/Form1.Designer.cs
+++ second1/Form1.Designer.cs
@@ -29,26 +29,15 @@ namespace second1
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(277, 99);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(247, 109);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Закрыть";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            this.ClientSize = new System.Drawing.Size(842, 302);
-            this.Controls.Add(this.button1);
+            this.BackColor = System.Drawing.Color.Green;
+            this.ClientSize = new System.Drawing.Size(676, 302);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.Name = "Form1";
             this.Text = "Блокнот ";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -57,8 +46,6 @@ namespace second1
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
     }
 }
 
